9354902c2bbfa340d0c0f155ad77725ad3adabb2
ntzes <50707431+ntzes@users.noreply.github.com>
2024-03-26 23:37:27 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,8 +2,18 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+#def hello():
+    #return "Hello World!"
+
+def salute_user():
+    # Prompt the user for their name
+    name = input("What's your name? ")
+
+    # Salute the user
+    print(f"Hello, {name}! Welcome!")
+
+# Call the function to salute the user
+salute_user()
 
 if __name__ == "__main__":
     app.run()
